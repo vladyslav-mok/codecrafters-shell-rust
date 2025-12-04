c5a5bcea0e88a9e165cf83239d7b27b724f3dab2
Vladyslav Mok <vladyslav.mok@openvpn.com>
2025-12-04 18:54:24 +0200

    pass 7st stage

--- src/main.rs
+++ src/main.rs
@@ -1,59 +1,39 @@
 #[allow(unused_imports)]
 use std::io::{self, Write};
+use std::process::exit;
 
-const BUILT_IN_COMMANDS: [&str; 3] = ["echo", "exit", "type"];
-
-enum Command {
-    ExitCommand,
-    EchoCommand { display_string: String },
-    TypeCommand { command_name: String },
-    CommandNotFound,
-}
-
-impl Command {
-    fn from_input(input: &str) -> Self {
-        let input = input.trim();
-        if input == "exit" {
-            return Self::ExitCommand;
-        };
-        if let Some(pos) = input.find("echo ") {
-            if pos == 0 {
-                return Self::EchoCommand {
-                    display_string: input["echo ".len()..].to_string(),
-                };
-            }
-        }
-        if let Some(pos) = input.find("type ") {
-            if pos == 0 {
-                return Self::TypeCommand {
-                    command_name: input["type ".len()..].to_string(),
-                };
-            }
-        }
-        Self::CommandNotFound
-    }
-}
+use pathsearch::find_executable_in_path;
 
 fn main() {
     loop {
+        let builtins = ["echo", "type", "exit"];
         print!("$ ");
         io::stdout().flush().unwrap();
-
         let mut input = String::new();
         io::stdin().read_line(&mut input).unwrap();
 
-        let command = Command::from_input(&input);
-        match command {
-            Command::ExitCommand => break,
-            Command::EchoCommand { display_string } => println!("{display_string}"),
-            Command::TypeCommand { command_name } => {
-                if BUILT_IN_COMMANDS.contains(&command_name.as_str()) {
-                    println!("{command_name} is a shell builtin");
+        if input.trim().is_empty() {
+            continue;
+        }
+
+        let mut parts = input.trim().split_whitespace();
+        let cmd = parts.next();
+        let args: Vec<&str> = parts.collect();
+
+        match cmd {
+            Some("exit") => exit(0),
+            Some("echo") => println!("{}", args.join(" ")),
+            Some("type") => {
+                if builtins.contains(&args.join(" ").as_str()) {
+                    println!("{} is a shell builtin", args.join(" "))
+                } else if let Some(path) = find_executable_in_path(&args.join(" ").as_str()) {
+                    println!("{} is {}", args.join(" "), path.to_str().unwrap());
                 } else {
-                    println!("{command_name}: not found");
+                    println!("{}: not found", args.join(" "))
                 }
             }
-            Command::CommandNotFound => println!("{}: command not found", input.trim()),
+            Some(huh) => println!("{}: command not found", huh),
+            None => println!(""),
         }
     }
 }
