1bcd92f5f44acdc047d20d2e46fd32f69bdf9848
Vladyslav Mok <vladyslav.mok@openvpn.com>
2025-12-04 18:35:59 +0200

    pass 6st stage

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,59 @@
 #[allow(unused_imports)]
 use std::io::{self, Write};
 
+const BUILT_IN_COMMANDS: [&str; 3] = ["echo", "exit", "type"];
+
+enum Command {
+    ExitCommand,
+    EchoCommand { display_string: String },
+    TypeCommand { command_name: String },
+    CommandNotFound,
+}
+
+impl Command {
+    fn from_input(input: &str) -> Self {
+        let input = input.trim();
+        if input == "exit" {
+            return Self::ExitCommand;
+        };
+        if let Some(pos) = input.find("echo ") {
+            if pos == 0 {
+                return Self::EchoCommand {
+                    display_string: input["echo ".len()..].to_string(),
+                };
+            }
+        }
+        if let Some(pos) = input.find("type ") {
+            if pos == 0 {
+                return Self::TypeCommand {
+                    command_name: input["type ".len()..].to_string(),
+                };
+            }
+        }
+        Self::CommandNotFound
+    }
+}
+
 fn main() {
     loop {
         print!("$ ");
         io::stdout().flush().unwrap();
-        let mut command = String::new();
-        io::stdin().read_line(&mut command).unwrap();
-        command = command.trim().to_string();
-        let parts = command.split_whitespace().collect::<Vec<&str>>();
-        match parts[0] {
-            "exit" => break,
-            "echo" => println!("{}", parts[1..].join(" ")),
-            _ => println!("{}: command not found", command),
+
+        let mut input = String::new();
+        io::stdin().read_line(&mut input).unwrap();
+
+        let command = Command::from_input(&input);
+        match command {
+            Command::ExitCommand => break,
+            Command::EchoCommand { display_string } => println!("{display_string}"),
+            Command::TypeCommand { command_name } => {
+                if BUILT_IN_COMMANDS.contains(&command_name.as_str()) {
+                    println!("{command_name} is a shell builtin");
+                } else {
+                    println!("{command_name}: not found");
+                }
+            }
+            Command::CommandNotFound => println!("{}: command not found", input.trim()),
         }
     }
 }
